663cfbf3a536cd01bbb3c42eaa7e817916d84472
shreydevep <shrey.devep@gmail.com>
2023-09-22 12:11:59 +0530

    Logout Element Added

--- frontend/bankapp/src/Components/Dashboard/Appbar.js
+++ frontend/bankapp/src/Components/Dashboard/Appbar.js
@@ -4,11 +4,17 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Appbar() {
+  const navigate = useNavigate();
   const customerId = sessionStorage.getItem("customerId");
+  const handleLogout = () => {
+    console.log("logout");
+    sessionStorage.clear();
+    navigate("/");
+  }
   return (
     <Box sx={{m:2, flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,7 +24,7 @@ export default function Appbar() {
             Wells Fargo
           </Typography>
           <Button color="inherit">{customerId}</Button>
-          <Button color="inherit">Logout</Button>
+          <Button color="inherit" onClick={()=>handleLogout()}>Logout</Button>
         </Toolbar>
       </AppBar>
     </Box>
